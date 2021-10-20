36405f85f8518b2396608d17fc69e06b54ec4643
Takacs Balazs <tbalazs.levente@gmail.com>
2021-10-20 17:50:11 +0200

    Játék

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -13,7 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView jatekosTipp, computerTipp;
     private Button koBtn, papirBtn, olloBtn;
     private TextView eredmenyText;
-    private Random rnd;
+    private static Random rnd;
+    private String jatekos;
+    private int pontComputer, pontJatekos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        koBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jatekosTipp.setImageResource(R.drawable.rock);
+                jatekos = "ko";
+            }
+        });
+
+        papirBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jatekosTipp.setImageResource(R.drawable.paper);
+                jatekos = "papir";
+            }
+        });
+
+        olloBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jatekosTipp.setImageResource(R.drawable.scissors);
+                jatekos = "ollo";
+            }
+        });
+
+        jatek(generateComputerTipp(), jatekos);
     }
-
-
 
     public void init() {
         jatekosTipp = findViewById(R.id.jatekosTipp);
@@ -31,5 +59,39 @@ public class MainActivity extends AppCompatActivity {
         koBtn = findViewById(R.id.koBtn);
         papirBtn = findViewById(R.id.papirBtn);
         olloBtn = findViewById(R.id.olloBtn);
+    }
+
+    public String generateComputerTipp() {
+        int random = rnd.nextInt((3) + 1) + 1;
+        String tipp = "";
+
+        switch (random) {
+            case 1:
+                computerTipp.setImageResource(R.drawable.rock);
+                tipp = "ko";
+                break;
+            case 2:
+                computerTipp.setImageResource(R.drawable.paper);
+                tipp = "papir";
+                break;
+            case 3:
+                computerTipp.setImageResource(R.drawable.scissors);
+                tipp = "ollo";
+                break;
+        }
+
+        return tipp;
+    }
+
+    public void jatek(String computer, String jatekos) {
+        if (computer.equals(jatekos)) {
+
+        } else if (computer.equals("ko") && jatekos.equals("papir")) {
+            Toast.makeText(MainActivity.this, "Győzelem", Toast.LENGTH_SHORT).show();
+        } else if (computer.equals("papir") && jatekos.equals("ollo")) {
+            Toast.makeText(MainActivity.this, "Győzelem", Toast.LENGTH_SHORT).show();
+        } else if (computer.equals("ollo") && jatekos.equals("ko")) {
+            Toast.makeText(MainActivity.this, "Győzelem", Toast.LENGTH_SHORT).show();
+        }
     }
 }
