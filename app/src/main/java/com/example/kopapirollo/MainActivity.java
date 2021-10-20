53e6b6d277e9377f55e02e27216b796b631bd905
Takacs Balazs <tbalazs.levente@gmail.com>
2021-10-20 17:17:20 +0200

    Inicializálás

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView jatekosTipp, computerTipp;
+    private Button koBtn, papirBtn, olloBtn;
+    private TextView eredmenyText;
+    private Random rnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+
+
+    public void init() {
+        jatekosTipp = findViewById(R.id.jatekosTipp);
+        computerTipp = findViewById(R.id.computerTipp);
+        koBtn = findViewById(R.id.koBtn);
+        papirBtn = findViewById(R.id.papirBtn);
+        olloBtn = findViewById(R.id.olloBtn);
     }
 }
